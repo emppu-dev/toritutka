5e111fc828676d4ff8a2e6a5ef731f1ee42c1244
emppu <83163481+emppu-dev@users.noreply.github.com>
2024-03-24 18:59:21 +0200

    Uudelleenkirjoitettu tukemaan tori betaa

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"io/ioutil"
 	"net/http"
 	"slices"
-	"strings"
 	"time"
 
 	"github.com/gocolly/colly"
@@ -16,6 +15,23 @@ import (
 type Config struct {
 	Hakusana string `json:"hakusana"`
 	Webhook  string `json:"webhook"`
+}
+
+type Product struct {
+	Heading      string   `json:"heading"`
+	Location     string   `json:"location"`
+	ImageURLs    []string `json:"image_urls"`
+	Timestamp    int64    `json:"timestamp"`
+	CanonicalURL string   `json:"canonical_url"`
+	Price        struct {
+		Amount       int    `json:"amount"`
+		CurrencyCode string `json:"currency_code"`
+		PriceUnit    string `json:"price_unit"`
+	} `json:"price"`
+}
+
+type Response struct {
+	Docs []Product `json:"docs"`
 }
 
 var foundTotal int
@@ -34,39 +50,50 @@ func main() {
 
 	c.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
+		r.Headers.Set("Accept", "application/json")
 	})
 
-	c.OnHTML("a.item_row_flex", func(h *colly.HTMLElement) {
-		found := h.Request.AbsoluteURL(h.Attr("href"))
-		imgUrl := ""
-		h.ForEach("img[src]", func(_ int, img *colly.HTMLElement) {
-			imgUrl = img.Attr("src")
-			ruleIndex := strings.Index(imgUrl, "?rule=")
-			if ruleIndex != -1 {
-				imgUrl = imgUrl[:ruleIndex+6] + "medium_660"
+	c.OnResponse(func(r *colly.Response) {
+		var response Response
+		//fmt.Println("Status code:", r.StatusCode)
+		if err := json.Unmarshal(r.Body, &response); err != nil {
+			fmt.Println("Error unmarshalling JSON:", err)
+			return
+		}
+		for i, product := range response.Docs {
+			if i >= 25 {
+				break
 			}
-		})
-		if !slices.Contains(seen, found) {
-			if !firstRun {
-				foundTotal++
-				sendWebhook(webhook, `{"content": null,"embeds": [{"title": "Uusi tuote löydetty 🔍","url": "`+found+`","description":"Hakusana: `+hakusana+`","color": 2895667,"image": {"url": "`+imgUrl+`"}}],"attachments": []}`)
+			if !slices.Contains(seen, product.CanonicalURL) {
+				if !firstRun {
+					foundTotal++
+					embeds := "\"embeds\": ["
+					for i := 0; i < len(product.ImageURLs) && i < 3; i++ {
+						embeds += fmt.Sprintf(`{"title": "%s","url": "%s","description":"Hinta: %d %s","color": 2895667,"image": {"url": "%s"}}`,
+							product.Heading, product.CanonicalURL, product.Price.Amount, product.Price.PriceUnit, product.ImageURLs[i])
+						if i < len(product.ImageURLs)-1 && i < 2 {
+							embeds += ","
+						}
+					}
+					embeds += "]"
+					payload := fmt.Sprintf(`{"content": null,%s,"attachments": []}`, embeds)
+					sendWebhook(webhook, payload)
+				}
+				seen = append(seen, product.CanonicalURL)
 			}
-			seen = append(seen, found)
 		}
 	})
-
-	c.OnRequest(func(r *colly.Request) {
-		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
-	})
-
+	fmt.Println("Hakusana:", hakusana)
 	for {
 		foundTotal = 0
-		c.Visit("https://www.tori.fi/koko_suomi?q=" + hakusana + "&cg=0&w=3&st=s&ca=18&l=0&md=th")
+		c.Visit("https://beta.tori.fi/recommerce-search-page/api/search/SEARCH_ID_BAP_COMMON?q=" + hakusana + "&sort=PUBLISHED_DESC")
+		c.Visit("https://beta.tori.fi/recommerce-search-page/api/search/SEARCH_ID_BAP_COMMON?q=" + hakusana + "&sort=PUBLISHED_DESC") // Ei toimi jos requestin lähettää vain kerran??!?!! 😭
 		if foundTotal > 0 {
 			fmt.Println("Uusia ilmoituksia löydetty: ", foundTotal)
 		}
-		time.Sleep(10 * time.Second)
+
 		firstRun = false
+		time.Sleep(10 * time.Second)
 	}
 }
 
